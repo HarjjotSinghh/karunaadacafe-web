d957187eed87c0a224794d6ad7024671f2edd221
Harjot Singh <HarjjotSinghh@gmail.com>
2024-08-08 00:28:38 +0530

    update: metadata SEO

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,17 +3,16 @@ import { SiteConfig } from "@/types"
 import { env } from "@/env.mjs"
 
 export const siteConfig: SiteConfig = {
-  name: "Next Entree",
-  author: "redpangilinan",
-  description:
-    "Next.js 14+ starter template with app router, shadcn/ui, typesafe env, icons and configs setup.",
-  keywords: ["Next.js", "React", "Tailwind CSS", "Radix UI", "shadcn/ui"],
+  name: "KarunaadaCafe",
+  author: "Harjot Singh",
+  description: "KarunaadaCafe is a cafe that serves delicious cuisine. It is based in Karnataka, India.",
+  keywords: ["KarunaadaCafe", "Cafe", "Cuisine", "Karnataka", "India", "Harjot Singh"],
   url: {
     base: env.NEXT_PUBLIC_APP_URL,
-    author: "https://rdev.pro",
+    author: "https://harjot.pro",
   },
   links: {
-    github: "https://github.com/redpangilinan/next-entree",
+    github: "https://github.com/HarjjotSinghh",
   },
   ogImage: `${env.NEXT_PUBLIC_APP_URL}/og.jpg`,
 }
